fad9ca919ad647b0dfea89eece23ddbe9da06e91
zero <dividebyzero@users.noreply.github.com>
2026-09-22 17:11:14 +0200

    Harden Auris switch and radio behavior

--- auris/radio.go
+++ auris/radio.go
@@ -10,23 +10,24 @@ import (
 type Radio struct {
 	widget.BaseWidget
 	Label string
-	Selected bool
+	Selected, Disabled bool
 	OnSelected func()
 }
 
 func NewRadio(label string, selected bool, changed func()) *Radio {
-	r := &Radio{Label: label, Selected: selected, OnSelected: changed}
+	r:=&Radio{Label:label,Selected:selected,OnSelected:changed}
 	r.ExtendBaseWidget(r)
 	return r
 }
-
+func (r *Radio) SetSelected(selected bool) { r.Selected=selected; r.Refresh() }
+func (r *Radio) SetDisabled(disabled bool) { r.Disabled=disabled; r.Refresh() }
 func (r *Radio) Tapped(*fyne.PointEvent) {
+	if r.Disabled { return }
+	r.Selected=true
+	r.Refresh()
 	if r.OnSelected != nil { r.OnSelected() }
 }
-
-func (r *Radio) CreateRenderer() fyne.WidgetRenderer {
-	return &radioRenderer{owner:r}
-}
+func (r *Radio) CreateRenderer() fyne.WidgetRenderer { return &radioRenderer{owner:r} }
 
 type radioRenderer struct { owner *Radio; objects []fyne.CanvasObject }
 func (r *radioRenderer) MinSize() fyne.Size { return fyne.NewSize(140,28) }
@@ -35,18 +36,16 @@ func (r *radioRenderer) Refresh() { r.rebuild(r.owner.Size()); canvas.Refresh(r.
 func (r *radioRenderer) Objects() []fyne.CanvasObject { return r.objects }
 func (r *radioRenderer) Destroy() {}
 func (r *radioRenderer) rebuild(size fyne.Size) {
-	s := DarkScheme()
-	border := s.BorderBright
-	if r.owner.Selected { border = s.PrimaryActive }
-	box := NewChamfer(s.SurfaceInset,border,s.Bevel.XS).Object(fyne.NewSize(18,18))
-	indicator := container.NewWithoutLayout(box)
-	indicator.Resize(fyne.NewSize(24,24))
+	s:=DarkScheme()
+	alpha:=uint8(0xff); if r.owner.Disabled { alpha=0x80 }
+	border:=s.BorderBright; if r.owner.Selected { border=s.PrimaryActive }
+	box:=NewChamfer(withColorAlpha(s.SurfaceInset,alpha),withColorAlpha(border,alpha),s.Bevel.XS).Object(fyne.NewSize(18,18))
+	indicator:=container.NewWithoutLayout(box); indicator.Resize(fyne.NewSize(24,24))
 	if r.owner.Selected {
-		pip := NewChamfer(s.PrimaryActive,s.PrimaryActive,2).Object(fyne.NewSize(8,8))
-		pip.Move(fyne.NewPos(5,5))
-		indicator.Add(pip)
+		pip:=NewChamfer(withColorAlpha(s.PrimaryActive,alpha),withColorAlpha(s.PrimaryActive,alpha),2).Object(fyne.NewSize(8,8))
+		pip.Move(fyne.NewPos(5,5)); indicator.Add(pip)
 	}
-	label := BodyText(r.owner.Label, 13, s.TextBright);
-row := container.NewHBox(indicator,label); row.Resize(size)
+	label:=BodyText(r.owner.Label,13,withColorAlpha(s.TextBright,alpha))
+	row:=container.NewHBox(indicator,label); row.Resize(size)
 	r.objects=[]fyne.CanvasObject{row}
 }
