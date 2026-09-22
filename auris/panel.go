a5a2a8156195c1d983231ccb6aad804705e6c376
zero <dividebyzero@users.noreply.github.com>
2026-09-22 16:40:36 +0200

    Tighten Auris geometry and intrinsic sizing

--- auris/panel.go
+++ auris/panel.go
@@ -1,23 +1,45 @@
 package auris
 
 import (
+	"image/color"
 	"strings"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 )
 
+func headerTick(left bool, c color.Color) fyne.CanvasObject {
+	const w, h, stroke float32 = 9, 9, 1
+	horizontal := canvas.NewRectangle(c)
+	vertical := canvas.NewRectangle(c)
+	horizontal.Resize(fyne.NewSize(w, stroke))
+	vertical.Resize(fyne.NewSize(stroke, h))
+	if !left {
+		vertical.Move(fyne.NewPos(w-stroke, 0))
+	}
+	group := container.NewWithoutLayout(horizontal, vertical)
+	group.Resize(fyne.NewSize(w, h))
+	return group
+}
+
 func NewPanel(title, code string, body fyne.CanvasObject, size fyne.Size, accent bool) fyne.CanvasObject {
 	s := DarkScheme()
 	titleColor := s.TextBright
-	if accent { titleColor = s.PrimaryActive }
+	if accent {
+		titleColor = s.PrimaryActive
+	}
 
-	left := DisplayText("⌐", 15, s.PrimaryDim)
-heading := DisplayText(strings.ToUpper(title), 15, titleColor)
-right := DisplayText("¬", 15, s.PrimaryDim)
-status := DataText(code, 11, s.TextMid)
-header := container.NewHBox(left, heading, right, layout.NewSpacer(), status)
+	heading := DisplayText(strings.ToUpper(title), 15, titleColor)
+	status := DataText(code, 11, s.TextMid)
+	header := container.NewHBox(
+		headerTick(true, s.PrimaryDim),
+		heading,
+		headerTick(false, s.PrimaryDim),
+		layout.NewSpacer(),
+		status,
+	)
 	inside := container.NewPadded(container.NewVBox(header, NewRule(size.Width), body))
 	return NewPanelSurface(inside, size, accent)
 }
